cf3fe44387e2377919e351071259cd636c27fd65
Алексей Щербаков <lexa.gb.2023@mail.ru>
2023-12-18 19:22:21 +1000

    Задача № 3

---
+++ Seminar_4_dz/Task_003/Class1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seminar_4_dz.Task_003
+{
+    class Class1
+    {
+        /// <summary>
+        /// Задача 3: Напишите программу, 
+        /// которая перевернёт одномерный массив
+        /// (первый элемент станет последним, второй – предпоследним и т.д.)
+        /// </summary>
+        /// <param name="Алескей Щербаков"></param>
+
+        Random rnd = new Random();
+        int[] number = new int[10];
+        static void Main(string[] args)
+        {
+            Class1 class1 = new Class1();
+            Console.WriteLine("Целочисленный массив рандомных чисел: ");
+            class1.ReverseArrayNumber();
+            Console.ReadLine();
+        }
+        void ReverseArrayNumber()
+        {
+            for (int i = 0; i < number.Length; i++)
+            {
+                number[i] = rnd.Next(1, 101);
+                Console.Write(number[i] + " ");
+            }
+            Console.WriteLine();
+            for (int i = 0; i < number.Length / 2; i++)
+            {
+                int res = number[i];
+                number[i] = number[number.Length - 1 - i];
+                number[number.Length - 1 - i] = res;
+            }
+            Console.WriteLine();
+            Console.WriteLine("Перевернутый массив: ");
+            foreach (var item in number)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
